26a707a67a676632942dacdf5bc0fe15c2c12b6c
Ikbenmathijs <51931123+Ikbenmathijs@users.noreply.github.com>
2023-10-24 13:29:12 +0200

    working on spreadsheet importer

---
+++ backend/src/sheetImporter/readSheet.ts
@@ -0,0 +1,11 @@
+import {Workbook} from "exceljs";
+
+
+
+const workbook = new Workbook();
+
+workbook.xlsx.readFile("./resources/dummy.xlsx").then(() => {
+    const sheet = workbook.getWorksheet("SE1");
+    console.log(sheet?.getRow(17).values);
+ });
+
