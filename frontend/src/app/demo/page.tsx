6742dd098f52f0e4d8121dbf9b6d32af2c956249
chloe <c>
2023-11-17 14:25:43 +0100

    a

--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -5,7 +5,7 @@ export default function demoPagina() {
 
     return (
         <div>
-            <h1>Hallo!</h1>
+            <h1 className="text-5xl bg-yellow-300">Hallo!</h1>
         </div>
     )
 }
